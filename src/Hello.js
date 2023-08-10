70509b0ee43f45bfe9c5cfe1209faf2a20341782
Hantaeyong <hhhhgong3@naver.com>
2023-08-10 14:31:40 +0900

    jsx 기초 학습2

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+//rafce
+
+import React from 'react';
+
+const Hello = () => {
+  return <div>Hello React</div>;
+};
+
+export default Hello;
---
+++ src/Noname.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Noname = () => {
+function Noname(){
+    return (
+        <div className='noname'>
+        <input type='text' />
+        <p>
+          오늘은 태풍이 와서 비가 많이 내렸다. <br />
+          그래서 공부가 하기 싫다.
+        </p>
+      </div>
+    );
+}
+}
+export default Noname;
