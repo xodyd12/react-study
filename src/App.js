440e16ff6cda40e68a18bb3d065143afee7ebac0
Hantaeyong <hhhhgong3@naver.com>
2023-08-10 14:11:23 +0900

    jsx 기초 학습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  // const $h2 = React.createElement('h2', null, '반가워요~~');
+  const $h2 = <h2>반가워요~</h2>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App">
+        <h1>메롱메롱</h1>
+        { $h2 }
+      </div>
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 태풍이 와서 비가 많이 내렸다. <br />
+          그래서 공부가 하기 싫다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
