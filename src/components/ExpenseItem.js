000639b88a5c83489d04761cee2bf044f82d2f0c
Hantaeyong <hhhhgong3@naver.com>
2023-08-11 09:46:59 +0900

    컴포넌트 분할하기 - ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,20 @@
 import React from 'react';
-// css 로딩
 import './ExpenseItem.css';
+import ExpensDate from './ExpenseDate';
 
-const ExpenseItem = ({title, price: propsPrice, date}) => {
-
-  // console.log(props);
-  // const price = 99999;
+const ExpenseItem = ({ title, price: prosPrice, date }) => {
+  // const price = 999;
 
   // const expenseDate = date;
   // const expenseTitle = title;
-  // const expensePrice = propsPrice;
+  // const expenseprice = prosPrice;
 
   // 1자리 숫자를 2자리수로 변환하는 함수
   const make2digit = (text) => {
     return text.toString().padStart(2, '0');
   };
 
-
-  // 날짜 포맷팅 변환 함수 정의
+  //날짜 포맷팅 변환 함수 정의
   const makeFormattedDate = () => {
     const year = date.getFullYear();
     const month = date.getMonth();
@@ -26,12 +23,11 @@ const ExpenseItem = ({title, price: propsPrice, date}) => {
     return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
   };
 
-  // 숫자를 원화표기법으로 바꾸기
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(propsPrice);
-
+  //숫자를 원화 표기법으로 바꾸기
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(prosPrice);
   return (
     <div className="expense-item">
-      <div>{makeFormattedDate()}</div>
+      <ExpensDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{formattedPrice}원</div>
