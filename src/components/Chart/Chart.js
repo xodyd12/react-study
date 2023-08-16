6cf1658d6a5ac7d08d8813ded29b3919e3c962c9
Hantaeyong <hhhhgong3@naver.com>
2023-08-16 11:40:49 +0900

    차트 렌더링

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+const Chart = ({ dataPoints }) => {
+  return (
+    <div className="chart">
+      {dataPoints.map((dp) => (
+        <ChartBar key={dp.label} label={dp.label} />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
