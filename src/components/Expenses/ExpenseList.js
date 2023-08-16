a8daf9c9d47874db8d8b1b3b745e37fb19ac5619
Hantaeyong <hhhhgong3@naver.com>
2023-08-16 09:40:07 +0900

    동적 리스트 렌더링

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,28 +1,47 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 
+const ExpenseList = ({ items }) => {
 
-const ExpenseList = ({items}) => {
+  // 선택된 연도 상태값 관리
+  const [filteredYear, setFilteredYear] = useState(new Date().getFullYear().toString());
+
+  // 자식 컴포넌트 ExpenseFilter에 있는 선택연도를 끌어올리는 콜백함수
+  const filterChangeHandler = (selectedYear) => {
+    // console.log('ExpenseList: ' + selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
+  // ExpenseItem을 동적 렌더링하기
+  // const iterateExpenseItem = () => {
+  //   return items.map((item) => (
+  //     <ExpenseItem
+  //       title={item.title}
+  //       price={item.price}
+  //       date={item.date}
+  //     />
+  //   ));
+  // };
+
   return (
-    <div className ="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        price={items[0].price}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        price={items[1].price}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        price={items[2].price}
-        date={items[2].date}
-      />
-    </div>
-  )
-}
+    <Card className="expenses">
+      <ExpenseFilter onChangeFilter={filterChangeHandler} />
 
-export default ExpenseList
+      {items
+        .filter(item => item.date.getFullYear().toString() === filteredYear)
+        .map(({ id, title, price, date }) => (
+        <ExpenseItem
+          key={id}
+          title={title}
+          price={price}
+          date={date}
+        />
+      ))}
+    </Card>
+  );
+};
+
+export default ExpenseList;
