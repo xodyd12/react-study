e62ce5e8dab11857ce9caa6314dada1c0088b001
Hantaeyong <hhhhgong3@naver.com>
2023-08-17 11:43:53 +0900

    동적 스타일 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -18,7 +18,7 @@ const CourseInput = ({onAdd}) => {
   const formSubmitHandler = (e) => {
     e.preventDefault();
     // console.log(enteredText);
-
+    //바보
     if (enteredText.trim().length === 0) {
       setIsValid(false);
       return;
@@ -30,13 +30,9 @@ const CourseInput = ({onAdd}) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
         <label>나의 목표</label>
         <input
-          style={{
-            background: isValid ? 'transparent' : 'salmon',
-            borderColor: isValid ? 'black' : 'red'
-          }}
           type="text"
           onChange={textChangeHandler}
           value={enteredText}
