1eaaf78f67597db72c5464eb21181ba829eb4302
Hantaeyong <hhhhgong3@naver.com>
2023-08-17 11:27:00 +0900

    동적 변경

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 import './CourseInput.css';
 import Button from '../UI/Button/Button';
 
-const CourseInput = ({ onAdd }) => {
+const CourseInput = ({onAdd}) => {
   const [enteredText, setEnteredText] = useState('');
+  // 입력값 검증 상태변수
+  const [isValid, setIsValid] = useState(true);
 
   const textChangeHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+    // console.log(e.target.value);
     setEnteredText(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(enteredText);
+    // console.log(enteredText);
+
+    if (enteredText.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
 
     onAdd(enteredText);
     setEnteredText('');
@@ -21,7 +32,15 @@ const CourseInput = ({ onAdd }) => {
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" onChange={textChangeHandler} value={enteredText} />
+        <input
+          style={{
+            background: isValid ? 'transparent' : 'salmon',
+            borderColor: isValid ? 'black' : 'red'
+          }}
+          type="text"
+          onChange={textChangeHandler}
+          value={enteredText}
+        />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
