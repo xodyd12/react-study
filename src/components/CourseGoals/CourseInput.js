f8653fda920fed415c575cffd87bbb8f1214da3f
Hantaeyong <hhhhgong3@naver.com>
2023-08-17 11:16:29 +0900

    목표설정 앱 - 기본구성(추가기능,삭제기능)

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import './CourseInput.css'
-import Button from '../UI/Button/Button'
-const CourseInput = () => {
-  return (
-    <form>
-    <div className="form-control">
-      <label>나의 목표</label>
-      <input type="text"/>
-    </div>
-    <Button type="submit">목표 추가하기</Button>
-  </form>
-  )
-}
+import React, { useState } from 'react';
+import './CourseInput.css';
+import Button from '../UI/Button/Button';
 
-export default CourseInput
+const CourseInput = ({ onAdd }) => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const textChangeHandler = (e) => {
+    setEnteredText(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredText);
+
+    onAdd(enteredText);
+    setEnteredText('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>나의 목표</label>
+        <input type="text" onChange={textChangeHandler} value={enteredText} />
+      </div>
+      <Button type="submit">목표 추가하기</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import './CourseList.css'
-const CourseList = () => {
-  return (
-    <ul className='goal-list'></ul>
-  )
-}
+import React from 'react';
+import './CourseList.css';
+import CourseItem from './CourseItem';
 
-export default CourseList
+const CourseList = ({ items,onDelete }) => {
+  return (
+    <ul className="goal-list">
+      {items.map((item) => {
+        return <CourseItem item={item} key={item.id} onDelete={onDelete} />;
+      })}
+    </ul>
+  );
+};
+
+export default CourseList;
