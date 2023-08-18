4c46bfca1c7292ee90c31195008515a78f378800
Hantaeyong <hhhhgong3@naver.com>
2023-08-18 09:24:13 +0900

    styled components 학습

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './CourseList.css';
+// import './CourseList.css';
 import CourseItem from './CourseItem';
+import styled from 'styled-components';
+
+const CourseUl = styled.ul`
+list-style: none;
+margin: 0;
+padding: 0;
+`;
 
 const CourseList = ({ items,onDelete }) => {
   return (
-    <ul className="goal-list">
+    <CourseUl>
       {items.map((item) => {
         return <CourseItem item={item} key={item.id} onDelete={onDelete} />;
       })}
-    </ul>
+    </CourseUl>
   );
 };
 
