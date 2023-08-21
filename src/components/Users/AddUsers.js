d12df58bfbcb7b1644f0260179a15534ac1b5a3d
Hantaeyong <hhhhgong3@naver.com>
2023-08-21 10:12:49 +0900

    useRef로 DOM 조작하기

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,79 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './AddUsers.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button/Button';
 import ErrorModal from '../UI/Modal/ErrorModal';
 
 const AddUsers = ({ onAddUser }) => {
-  const [userValue, setUserValue] = useState({
-    username: '',
-    age: '',
-  });
+
+  // 에러 상태 관리
   const [error, setError] = useState(null);
 
-  const usernameChangeHandler = (e) => {
-    setUserValue((preUserValue) => ({
-      ...preUserValue,
-      username: e.target.value,
-    }));
-  };
-  const ageChangeHandler = (e) => {
-    setUserValue((preUserValue) => ({
-      ...preUserValue,
-      age: e.target.value,
-    }));
-  };
+  // 인풋 dom 가져오기
+  const nameInput = useRef();
+  const ageInput = useRef();
+
   const userSubmitHandler = (e) => {
     e.preventDefault();
 
-    //입력값 검증
-    if (userValue.username.trim() === '' || userValue.age.trim() === '') {
+    console.log(nameInput.current);
+
+    const username = nameInput.current.value;
+    const age = ageInput.current.value;
+
+    // 입력값 검증
+    if (username.trim() === '' || age.trim() === '') {
       setError({
         title: '유효하지 않은 입력값',
-        message: '입력값은 공백으로 작성하면 안됩니다!',
+        message: '입력값은 공백으로 작성하면 안됩니다!'
       });
       return;
     }
-    if (+userValue.age < 1) {
+
+    if (+age < 1) {
       setError({
         title: '유효하지 않은 나이의 범위',
-        message: '나이는 1 이상의 숫자로 작성해주세요',
+        message: '나이는 1 이상의 숫자로 작성해주세요!'
       });
       return;
     }
-    console.log(userValue);
-    onAddUser(userValue);
 
-    setUserValue({
-      username: '',
-      age: '',
-    });
+    onAddUser({username, age});
+
+    nameInput.current.value = '';
+    ageInput.current.value = '';
+    
   };
 
   return (
-    <>
-      {error && (
-        <ErrorModal
-          title={error.title}
-          message={error.message}
-          onConfirm={() => setError(null)}
-        />
-      )}
+    // React.Fragment
+    <> 
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={() => setError(null)} />}
       <Card className={styles.input}>
         <form onSubmit={userSubmitHandler}>
           <label htmlFor="username">이름</label>
           <input
             id="username"
             type="text"
-            onChange={usernameChangeHandler}
-            value={userValue.username}
+            ref={nameInput}
           />
           <label htmlFor="age">나이</label>
           <input
             id="age"
             type="number"
-            onChange={ageChangeHandler}
-            value={userValue.age}
+            ref={ageInput}
           />
           <Button type="submit">가입하기</Button>
         </form>
