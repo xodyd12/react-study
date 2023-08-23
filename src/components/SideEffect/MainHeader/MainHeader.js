b627e6948e4a84f6bcfcea723360ba3e245f2515
Hantaeyong <hhhhgong3@naver.com>
2023-08-23 10:27:47 +0900

    useContext 와 리팩토링

--- src/components/SideEffect/MainHeader/MainHeader.js
+++ src/components/SideEffect/MainHeader/MainHeader.js
@@ -3,14 +3,10 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import classes from './MainHeader.module.css';
 
-const MainHeader = (isAuthenticated, onLogout) => {
+const MainHeader = () => {
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation
-        isLoggedIn={isAuthenticated}
-        onLogout={onLogout}
-      />
     </header>
   );
 };
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+//로그인 상태변수를 관리할 컨텍스트
 
-// 로그인 상태 변수를 관리할 컨텍스트 
 const AuthContext = React.createContext({
-    isLoggedIn: false,
-    
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (emain, password) => {},
 });
+
+export const AuthContextProvider = ({children}) => {
+  //로그인 상태를 관리하는 변수
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //화면이 리 렌더링될때 localStorage를 확인해서
+  //현재 로그인 플래그가 존재하는지 검사
+  useEffect(() => {
+    console.log('로그인 검사 수행');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+   //서버로 로그인을 요청하는 함수
+   const loginHandler = (email, password) => {
+    //로그인의 증거로 브라우저에 로그인 값을 1로 표현해서 저장
+    localStorage.setItem('login-flag', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setIsLoggedIn(false);
+  };
+  return(
+    <AuthContext.Provider value={{
+      isLoggedIn:isLoggedIn,
+      onLogout : logoutHandler,
+      onLogin:loginHandler,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+};
 
 export default AuthContext;
